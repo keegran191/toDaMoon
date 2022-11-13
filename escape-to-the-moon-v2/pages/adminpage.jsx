b910709da32f9c32251379c0e3b53790d109036a
M1zTerP <sawagan@hotmail.com>
2022-11-13 19:33:56 +0700

    Update adminpage.jsx

--- escape-to-the-moon-v2/pages/adminpage.jsx
+++ escape-to-the-moon-v2/pages/adminpage.jsx
@@ -3,6 +3,9 @@ import style from '../styles/Admin.module.css'
 import Link from 'next/link'
 import NavAdmin from '../components/NavbarAdmin.js'
 
+import { useState } from 'react'
+import { useRouter } from 'next/router'
+
 export default function admin() {
 
     return(
@@ -14,12 +17,10 @@ export default function admin() {
                 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin/>
                 <link href="https://fonts.googleapis.com/css2?family=Kanit&display=swap" rel="stylesheet"/>
             </Head>
-            <main>
-                <NavAdmin></NavAdmin>
-                <div class={style.adminContainer}>
-                    
-                </div>
-            </main>
+            <NavAdmin></NavAdmin>
+            <div class={style.adminContainer}>
+                
+            </div>
         </div>
     )
 }
