3c5ba0a0b16e315e0165492765d42d72c673236c
keegran <keegran191@gmail.com>
2022-11-23 15:59:44 +0700

    cate_login_midwae

--- escape-to-the-moon-v2/middleware.ts
+++ escape-to-the-moon-v2/middleware.ts
@@ -5,9 +5,9 @@ export default function middleware(req:NextRequest){
     let verify = req.cookies.get("loggedin");
     let url = req.url
     
-    if(!verify && url.includes('/adminpage')){
-        return NextResponse.redirect("http://localhost:3000/login");
-    }
+    //if(!verify && url.includes('/adminpage')){
+    //    return NextResponse.redirect("http://localhost:3000/login");
+    //}
 
     if (verify && url === "http://localhost:3000/login") {
       return NextResponse.redirect("http://localhost:3000/");
