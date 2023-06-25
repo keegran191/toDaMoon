b4593d1e4654f3fdb65b21f9674ba6fbb51a4e66
keegran <keegran191@gmail.com>
2023-06-26 03:18:40 +0700

    Delete SubCat

--- escape-to-the-moon-v2/pages/api/subcategory/delete/[scid].js
+++ escape-to-the-moon-v2/pages/api/subcategory/delete/[scid].js
@@ -1,14 +1,26 @@
 import pool from "../../../../lib/database";
 
 export default async function handler(req, res) {
+
   const { scid } = req.query;
 
-  const [results] = await pool.query('DELETE FROM subcategory WHERE sub_id = ?',[scid]).catch((err) => {
+  const [results] = await pool.query('SELECT * FROM stock WHERE SubCategoryId = ? ',[scid]).catch((err) => {
     res.status(500).json({ "Status": "Database Error" });
     console.error(err);
-    return null;
   });
 
-  console.log("Delete success")
-  res.status(201).json({"Status": "Sub Category Deleted"});
+  if (results.length > 0) {
+    console.log('using')
+    res.redirect(307, "/adminpage/subcategory?errorMsg=SubCaregoryIdIsUsing=");
+
+  } else {
+    const [resultsSubCategory] = await pool.query('DELETE FROM subcategory WHERE sub_id = ?',[scid]).catch((err) => {
+      res.status(500).json({ "Status": "Database Error" });
+      console.error(err);
+    });
+  
+    console.log("Delete success")
+    res.redirect(307, "/adminpage/subcategory");
+  }
+  
 }
