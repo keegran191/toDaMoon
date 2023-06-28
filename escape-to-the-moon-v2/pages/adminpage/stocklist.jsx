e4ea3d91cac38171298937e4d09e2bd9bc75c12d
M1zTerP <sawagan@hotmail.com>
2023-06-29 01:58:31 +0700

    api list unfinish yet

--- escape-to-the-moon-v2/pages/adminpage/stocklist.jsx
+++ escape-to-the-moon-v2/pages/adminpage/stocklist.jsx
@@ -2,24 +2,30 @@ import Head from 'next/head'
 import style from '../../styles/Admin.module.css'
 import NavAdmin from '../../components/NavbarAdmin.js'
 import Axios from 'axios';
-import { useState, useEffect, createRef } from 'react';
-import Item from '../../components/Item.js';
+import { useState, useEffect } from 'react';
+import UniversalModal from '../../components/Modal.js';
+import React from 'react'
 import Select from 'react-select'
 
 function Stock() {
     
     const [stockList, setStockList] = useState([])
+    const [search, setSearch] = useState("")
 
-    const GetStokList = () => {
+    const GetStokcList = () => {
         Axios.get("http://localhost:3000/api/stock/getallstock").then((response) => {
             setStockList(response.data);
         })
     }
 
-    useEffect(() => {
-        GetStokList()
-    });
+    const searchItem = (e) => {
+        const value = e.target.value;
+        setSearch(value);
+    }
 
+    useEffect(() => {
+        GetStokcList()
+    }, []);
 
     return (
         <div>
@@ -31,8 +37,21 @@ function Stock() {
                 <link href="https://fonts.googleapis.com/css2?family=Kanit&display=swap" rel="stylesheet"/>
             </Head>
             <NavAdmin></NavAdmin>
+            <div className="px-80">
+                <div className="px-80 mt-24">
+                    <div className="relative mt-10">
+                        <input onChange={searchItem} id="Search" name="Search" className="block p-4 pl-5 w-full text-md text-[#252525] bg-[#FFFFFF] rounded-full border border-[#252525]" placeholder="ค้าหาสินค้า"></input>
+                        <button className="text-white absolute right-2.5 bottom-2.5 bg-[#252525] hover:bg-[#252525] font-medium rounded-full text-sm px-6 py-2">ค้นหา</button>
+                    </div>
+                </div>
 
-
+                {stockList && stockList.map((post) => {
+                        return <div className="" key={post.id}>
+                            {post.Title}
+                        </div> 
+                })}
+            </div>
+            
         </div>
     )
 }
--- escape-to-the-moon-v2/pages/api/stock/getallstock.js
+++ escape-to-the-moon-v2/pages/api/stock/getallstock.js
@@ -1,11 +1,9 @@
 import pool from "../../../lib/database";
 
 export default async function handler(req, res) {
-
-  const [results] = await pool.query('SELECT * FROM stock', []).catch((err) => {
+  const [results] = await pool.query('SELECT * FROM stock',[]).catch((err) => {
     res.status(500).json({ "Status": "Database Error" });
     console.error(err);
-    return null;
   });
 
   res.status(200).json(results);
