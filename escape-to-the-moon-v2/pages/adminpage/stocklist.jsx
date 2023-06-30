5d4dc519b029f96aeee8119a0010549e1c628766
M1zTerP <sawagan@hotmail.com>
2023-07-01 00:06:00 +0700

    config item layout

--- escape-to-the-moon-v2/pages/adminpage/stocklist.jsx
+++ escape-to-the-moon-v2/pages/adminpage/stocklist.jsx
@@ -7,7 +7,7 @@ import UniversalModal from '../../components/Modal.js';
 import React from 'react';
 import Item from '../../components/Item.js';
 import { motion, AnimatePresence } from 'framer-motion';
-
+import Select from 'react-select'
 function StockConfig() {
     //Stock List
     const [stockList, setStockList] = useState([]);
@@ -152,6 +152,7 @@ function StockConfig() {
 
     useEffect(() => {
         GetStokcList();
+        GetCoffee()
         GetCategory()
         GetProcess(42)
         GetRoast(40)
@@ -199,66 +200,66 @@ function StockConfig() {
             </motion.button>
           </div>
 
-            <div className={`mt-9 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 xl:grid-cols-4 2xl:grid-cols-5 gap-6 justify-items-center ${selectedId ? style.blurBackground : ''}`}>
+            <div className={`overflow-y-auto overflow-x-hidden h-4/6 w-fill mt-9 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 xl:grid-cols-4 2xl:grid-cols-5 gap-2 justify-items-center ${selectedId ? style.blurBackground : ''}`}>
                 {stockList.map((post) => (
-                <motion.div
-                    key={post.Id}
-                    className="select-none w-48 h-52 bg-white rounded-xl shadow-lg flex flex-col justify-between p-4 cursor-pointer"
-                    whileHover={{ scale: 1.05, transition: { duration: 0.2 } }}
-                    layoutId={post.Id}
-                    onClick={() => {
-                        setSelectedId(post.Id)
-                        setTitle(post.Title)
-                        setDetail(post.Detail)
-                        setAmount(post.Amount)
-                        setPrice(post.Price)
-                        setIsAdvise(post.IsAdvise)
-                        setStockType(post.StockType)
-                        setProcess(post.Process)
-                        setRoast(post.Roast)
-                        setFlavor(post.Flavor)
-                        setCategoryId(post.CategoryId)
-                        setSubCategoryId(post.SubCategoryId)
-                    }}
-                >
-                    <div className="flex justify-center items-center">
-                    <img src={`/uploads/${post.Image}`} alt={post.Title} className="w-32 h-32" />
-                    </div>
-                    <div className="px-4 mt-1">
-                    <h3 className="text-lg font-semibold">{post.Title}</h3>
-                    <p className="text-gray-600 overflow-hidden overflow-ellipsis whitespace-nowrap">฿{post.Price}</p>
-                    </div>
-                </motion.div>
+                    <motion.div
+                        key={post.Id}
+                        className="select-none w-48 h-52 bg-white rounded-xl shadow-lg flex flex-col justify-between p-4 cursor-pointer"
+                        whileHover={{ scale: 1.05, transition: { duration: 0.2 } }}
+                        layoutId={post.Id}
+                        onClick={() => {
+                            setSelectedId(post.Id)
+                            setTitle(post.Title)
+                            setDetail(post.Detail)
+                            setAmount(post.Amount)
+                            setPrice(post.Price)
+                            setIsAdvise(post.IsAdvise)
+                            setStockType(post.StockType)
+                            setProcess(post.Process)
+                            setRoast(post.Roast)
+                            setFlavor(post.Flavor)
+                            setCategoryId(post.CategoryId)
+                            setSubCategoryId(post.SubCategoryId)
+                        }}
+                    >
+                        <div className="flex justify-center items-center">
+                        <img src={`/uploads/${post.Image}`} alt={post.Title} className="w-32 h-32" />
+                        </div>
+                        <div className="px-4 mt-1">
+                        <h3 className="text-lg font-semibold">{post.Title}</h3>
+                        <p className="text-gray-600 overflow-hidden overflow-ellipsis whitespace-nowrap">฿{post.Price}</p>
+                        </div>
+                    </motion.div>
                 ))}
                 <AnimatePresence>
                 {selectedId && (
                 //<motion.div layoutId={selectedId} className={`fixed top-0 left-0 right-0 bottom-0 flex items-center justify-center select-none bg-white rounded-xl shadow-lg ${style.selectedItem}`}></motion.div>
-                <motion.div layoutId={selectedId} className={`absolute top-52 select-none w-4/6 h-4/6 bg-white rounded-xl shadow-lg flex flex-col p-4 ${style.selectedItem}`}>
-                    <motion.button
-                        onClick={() => {
-                            setSelectedId(null)
-                            GetStokcList();
-                        }}
-                        className="self-end text-gray-600 text-sm font-medium"
-                        >
-                            X
+                    <motion.div layoutId={selectedId} className={`absolute top-36 select-none w-4/6 h-4/6 bg-white rounded-xl shadow-lg flex flex-col p-4 ${style.selectedItem}`}>
+                        <motion.button
+                            onClick={() => {
+                                setSelectedId(null)
+                                GetStokcList();
+                            }}
+                            className="self-end text-gray-600 text-sm font-medium"
+                            >
+                                X
                         </motion.button>
-                    <div className="flex px-4">
-                        <div className="flex px-4 py-10">
-                            <img src={`/uploads/${selectedPost.Image}`} alt={selectedPost.Title} className="w-64 h-64" />
-                            {/* <div className="relative plusContainer top-1/2 transform -translate-y-1/2">
-                                {imagesURLs.length < 1 && <label className="relative hover:cursor-pointer" htmlFor="image"><span className='text-5xl'>+</span></label>}
-                                {imagesURLs.length > 0 && imagesURLs.map((imageSrc, idx) =>
-                                    <div className="img">
-                                        <img key={idx} className='h-64 w-64 transition duration-300 ease-in-out transform hover:opacity-50' onClick={() => {
-                                            document.getElementById('image').click();
-                                        }} src={imageSrc}></img>
-                                    </div>
-                                )}
-                            </div> */}
-                            {/* <input className="hidden" type="file" id='image' name="image" accept='image/*' onChange={onImageChanged}/> */}
-                        </div> 
-                        <div className="px-4 mt-1 ml-4">
+                        <div className="flex px-4">
+                            <div className="flex px-4 py-10">
+                                <img src={`/uploads/${selectedPost.Image}`} alt={selectedPost.Title} className="w-64 h-64" />
+                                {/* <div className="relative plusContainer top-1/2 transform -translate-y-1/2">
+                                    {imagesURLs.length < 1 && <label className="relative hover:cursor-pointer" htmlFor="image"><span className='text-5xl'>+</span></label>}
+                                    {imagesURLs.length > 0 && imagesURLs.map((imageSrc, idx) =>
+                                        <div className="img">
+                                            <img key={idx} className='h-64 w-64 transition duration-300 ease-in-out transform hover:opacity-50' onClick={() => {
+                                                document.getElementById('image').click();
+                                            }} src={imageSrc}></img>
+                                        </div>
+                                    )}
+                                </div>
+                                <input className="hidden" type="file" id='image' name="image" accept='image/*' onChange={onImageChanged}/> */}
+                            </div> 
+                            <div className="px-4 mt-1 ml-4">
                                 <div className="D">
                                     <label className="">ชื่อสินค้า</label>
                                     <input value={Title} maxLength={20} className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg block w-full p-2.5" placeholder="ชื่อสินค้า" required onChange={(event) => {setTitle(event.target.value)}}></input>
@@ -311,8 +312,101 @@ function StockConfig() {
                                         </label>
                                     </div>
                                 </div>
+                                <div className="mt-4 flex flex-wrap justify-between">
+                                    <div className="w-full md:w-1/2 sm:pr-2">
+                                        <label className="">ชนิดสินค้า</label>
+                                        <Select
+                                            inputId='coffeeId'
+                                            options={optionCoffee}
+                                            onChange={(newValue,meta) => {
+                                                setStockType(newValue.value); 
+                                            }}
+                                            styles={customStyles}
+                                            placeholder="เลือกชนิดสินค้า"
+                                        />
+                                    </div>
+                                    
+                                    {StockType == 1 && 
+                                        <div className="w-full md:w-1/2 sm:pl-2">
+                                            <label className="">วิธีการแปรรูป</label>
+                                            <Select
+                                                inputId='processId'
+                                                options={coffeeProcess}
+                                                onChange={(newValue,meta) => {
+                                                    setProcess(newValue.value)
+                                                }}
+                                                styles={customStyles}
+                                                placeholder="เลือกวิธีการแปรรูป"
+                                            />
+                                        </div>
+                                    }
+
+                                    {StockType == 1 && 
+                                        <div className="w-full md:w-1/2 sm:pr-2 sm:mt-2">
+                                            <label className="">วิธีการคั่ว</label>
+                                            <Select
+                                                inputId='roastId'
+                                                options={coffeeRoast}
+                                                onChange={(newValue,meta) => {
+                                                    setRoast(newValue.value)
+                                                }}
+                                                styles={customStyles}
+                                                placeholder="เลือกวิธีการคั่ว"
+                                            />
+                                        </div>
+                                    }
+
+                                    {StockType == 1 && 
+                                        <div className="w-full md:w-1/2 sm:pl-2 sm:mt-2">
+                                            <label className="">กลื่น รส</label>
+                                            <Select
+                                                inputId='flavorId'
+                                                options={coffeeFlavor}
+                                                onChange={(newValue,meta) => {
+                                                    setFlavor(newValue.value)
+                                                }}
+                                                styles={customStyles}
+                                                placeholder="เลือกกลิ่น รส"
+                                            />
+                                        </div>
+                                    }
+
+                                    {StockType == 2 &&
+                                        <div className="w-full md:w-1/2 sm:pl-2">
+                                            <label className="">ประเภทสินค้า</label>
+                                            <Select
+                                                inputId='categoryId'
+                                                options={optionCategory}
+                                                onChange={(newValue,meta) => {
+                                                    setCategoryId(newValue.value)
+                                                    GetSubCategory(newValue.value);
+                                                }}
+                                                styles={customStyles}
+                                                placeholder="เลือกประเภทสินค้า"
+                                            />
+                                        </div>
+                                    }
+
+                                    {StockType == 2 && CategolyId &&
+                                        <div className="w-full md:w-1/2 sm:pr-2 sm:mt-2">
+                                            <label className="">หมวดหมู่สินค้า</label>
+                                            <Select
+                                                inputId='categoryId'
+                                                options={optionSubCategory}
+                                                onChange={(newValue,meta) => {
+                                                    setSubCategoryId(newValue.value)
+                                                }}
+                                                styles={customStyles}
+                                                placeholder="เลือกหมวดหมู่สินค้า"
+                                            />
+                                        </div>
+                                    }
+                                </div>
+                                <div className="button-container mt-5 text-center">
+                                    <button type="submit" className="text-white bg-[#252525] hover:bg-[#010101] font-medium rounded-lg text-sm px-5 py-2.5 mr-4 mb-4 transition duration-300 ease-in-out transform hover:scale-125">เพิ่มสินค้า</button>
+                                </div>
+                            </div>
                         </div>
-                    </div>
                     </motion.div>
                 )}
                 </AnimatePresence>
@@ -323,3 +417,5 @@ function StockConfig() {
 }
 
 export default StockConfig;
+
+                                    
