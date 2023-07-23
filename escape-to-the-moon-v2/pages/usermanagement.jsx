8ea8b826829773bd291d7ce87ae614572a0af46e
M1zTerP <sawagan@hotmail.com>
2023-07-23 20:16:48 +0700

    Update usermanagement.jsx

--- escape-to-the-moon-v2/pages/usermanagement.jsx
+++ escape-to-the-moon-v2/pages/usermanagement.jsx
@@ -16,6 +16,7 @@ function UserManagement ({ cookies}) {
     const [stockAmount, setStockAmount] = useState(0)
 
     const [toggleUser, setToggleUser] = useState(false)
+    const [toggleHistory, setToggleHistory] = useState(false)
 
     const [changeUser, setChangeUser] = useState(false)
     const [changePassword, setChangePassword] = useState(false)
@@ -43,6 +44,7 @@ function UserManagement ({ cookies}) {
 
     //Rotare State
     const [rotateUser, setRotateUser] = useState(0);
+    const [rotateHistory, setRotateHistory] = useState(0);
 
     const filterFnameTextInput = (e) => {
         const value = e.target.value;
@@ -214,6 +216,37 @@ function UserManagement ({ cookies}) {
                         </svg>
                         <span>เเก้ไขรหัสผ่าน</span>
                     </motion.div>}
+
+                    <motion.div 
+                        className='w-full flex justify-start cursor-pointer border-2 border-[#25252500] p-4'
+                        onClick={() => {
+                            if(toggleHistory) {
+                                setToggleHistory(false);
+                                setRotateHistory(0)
+                            } else {
+                                setRotateHistory(180);
+                                setToggleHistory(true);
+                            }
+                        }}
+                    >
+                        <svg className='sm:w-4 sm:h-4 w-3 h-3 sm:mr-2 ml-1 m-auto fill-[#252525]' xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 576 512">
+                            <path d="M253.3 35.1c6.1-11.8 1.5-26.3-10.2-32.4s-26.3-1.5-32.4 10.2L117.6 192H32c-17.7 0-32 14.3-32 32s14.3 32 32 32L83.9 463.5C91 492 116.6 512 146 512H430c29.4 0 55-20 62.1-48.5L544 256c17.7 0 32-14.3 32-32s-14.3-32-32-32H458.4L365.3 12.9C359.2 1.2 344.7-3.4 332.9 2.7s-16.3 20.6-10.2 32.4L404.3 192H171.7L253.3 35.1zM192 304v96c0 8.8-7.2 16-16 16s-16-7.2-16-16V304c0-8.8 7.2-16 16-16s16 7.2 16 16zm96-16c8.8 0 16 7.2 16 16v96c0 8.8-7.2 16-16 16s-16-7.2-16-16V304c0-8.8 7.2-16 16-16zm128 16v96c0 8.8-7.2 16-16 16s-16-7.2-16-16V304c0-8.8 7.2-16 16-16s16 7.2 16 16z"/>
+                        </svg>
+                        <span>การสั่งซื้อ</span>
+                        <motion.div
+                            className='sm:w-4 sm:h-4 w-3 h-3 sm:mr-2 m-auto '
+                            animate={{
+                                x: 0,
+                                y: 0,
+                                scale: 1,
+                                rotate: rotateHistory,
+                            }}
+                        >
+                            <svg className='fill-[#252525]' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
+                                <path d="M201.4 342.6c12.5 12.5 32.8 12.5 45.3 0l160-160c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L224 274.7 86.6 137.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3l160 160z"/>            
+                            </svg>
+                        </motion.div>
+                    </motion.div>
 
                     <div className='flex justify-center cursor-pointer mt-48'>
                         <motion.button 
