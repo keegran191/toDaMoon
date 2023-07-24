acca88afaf974759061f21d80212693da45c98a9
keegran <keegran191@gmail.com>
2023-07-24 21:10:10 +0700

    store/filter

--- escape-to-the-moon-v2/pages/api/store/filter.js
+++ escape-to-the-moon-v2/pages/api/store/filter.js
@@ -1,13 +1,20 @@
 import pool from "../../../lib/database";
 
 export default async function handler(req, res) {
-  const { search } = req.query.search;
-  const { stocktype } = req.query.stocktype
   
+  const  search  = req.query.search;
+  const { stocktype } = req.query.Stocktype
+  const { process } = req.query.Process
+  const { roast } = req.query.Roast
+  const { flavor } = req.query.Flavor
+  const { categoly } = req.query.CategolyId
+  
+  console.log(search)
+
   try {
     const [results] = await pool.query(
       'SELECT * FROM stock WHERE Title LIKE ? OR StockType LIKE ? OR Process LIKE ? OR Roast LIKE ? OR Flavor LIKE ? OR CategoryId LIKE ? OR SubCategoryId LIKE ?',
-      [`%${search}%`, `%${stocktype}%`, `%${search}%`,`%${search}%`, `%${search}%`, `%${search}%`]
+      [`%${search}%`, `%${stocktype}%`, `%${process}%`,`%${roast}%`, `%${flavor}%`, `%${categoly}%`]
     );
 
     res.status(200).json(results);
