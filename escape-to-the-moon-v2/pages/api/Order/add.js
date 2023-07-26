2331bc0ecf42882ac35abfbd2df0af8d88f9a641
M1zTerP <sawagan@hotmail.com>
2023-07-26 20:27:44 +0700

    update api

--- escape-to-the-moon-v2/pages/api/Order/add.js
+++ escape-to-the-moon-v2/pages/api/Order/add.js
@@ -4,8 +4,9 @@ export default async function handler(req, res) {
 
     const refNumber = Date.now();
     const addressId = req.query.addressId
+    const UserId = req.query.UserId
 
-    const [results] = await pool.query('INSERT INTO order_list (refNumber, addressId) VALUES (?,?)',[refNumber,addressId]).catch((err) => {
+    const [results] = await pool.query('INSERT INTO order_list (refNumber, addressId, UserId) VALUES (?,?,?)',[refNumber,addressId,UserId]).catch((err) => {
         res.status(500).json({ "Status": "Database Error" });
         console.error(err);
         return null;
