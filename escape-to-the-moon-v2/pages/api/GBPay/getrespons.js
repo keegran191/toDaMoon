a52ce1038cd64aba5fc51c1245cffa66e76ea08b
keegran <keegran191@gmail.com>
2023-07-26 23:31:09 +0700

    Update get res 2.0

--- escape-to-the-moon-v2/pages/api/GBPay/getrespons.js
+++ escape-to-the-moon-v2/pages/api/GBPay/getrespons.js
@@ -8,42 +8,58 @@ export default async function handler(req, res) {
         return null;
     });
     
-    const resultRows = await pool.query('SELECT UserId, order_Id FROM order_list WHERE refNumber = ?', [req.body.referenceNo]).catch((err) => {
-        res.status(500).json({ "Status": "Database Error" });
+    const resultRows = await pool
+        .query("SELECT UserId, order_Id FROM order_list WHERE refNumber = ?", [
+        req.body.referenceNo,
+        ])
+        .catch((err) => {
+        res.status(500).json({ Status: "Database Error" });
         console.error(err);
         return [];
-      });
-      
-    const resultsStock = await pool.query('SELECT stockId, stockAmount, stockPrice FROM basket WHERE userId = ?', [resultRows.UserId]).catch((err) => {
-        res.status(500).json({ "Status": "Database Error" });
+        });
+
+    if (resultRows.length === 0) {
+        res.status(404).json({ Status: "Order not found" });
+        return;
+    }
+
+    const userId = resultRows[0].UserId;
+    const orderId = resultRows[0].order_Id;
+
+    const resultsStock = await pool
+        .query("SELECT stockId, stockAmount, stockPrice FROM basket WHERE userId = ?", [
+        userId,
+        ])
+        .catch((err) => {
+        res.status(500).json({ Status: "Database Error" });
         console.error(err);
         return [];
-      });
-      
-      // Combine order_Id with each row of resultsStock
-    const combinedResults = resultsStock.map(stockItem => {
-        return { ...stockItem, order_Id: resultRows.order_Id };
-      });
-      
-      // Insert each row of combinedResults into the item_order table
-    combinedResults.forEach(async item => {
-        const { order_Id, stockId, stockAmount, stockPrice } = item;
+        });
+
+    // Insert each row of resultsStock into the item_order table
+    for (const stockItem of resultsStock) {
+        const { stockId, stockAmount, stockPrice } = stockItem;
         const total = stockAmount * stockPrice;
-      
-        const insertQuery = 'INSERT INTO item_order (order_id, stock_id, price, amount, total) VALUES (?, ?, ?, ?, ?)';
-        await pool.query(insertQuery, [order_Id, stockId, stockPrice, stockAmount, total]).catch((err) => {
-            res.status(500).json({ "Status": "Database Error" });
+
+        const insertQuery =
+        "INSERT INTO item_order (order_id, stock_id, price, amount, total) VALUES (?, ?, ?, ?, ?)";
+        await pool
+        .query(insertQuery, [orderId, stockId, stockPrice, stockAmount, total])
+        .catch((err) => {
+            res.status(500).json({ Status: "Database Error" });
             console.error(err);
             return [];
         });
-    });
-      
-    // Clear items in the basket 
-    const ClearBasket = await pool.query('DELETE FROM basket WHERE userId = ?', [resultRows.UserId]).catch((err) => {
-        res.status(500).json({ "Status": "Database Error" });
+    }
+
+    // Clear items in the basket
+    const ClearBasket = await pool
+        .query("DELETE FROM basket WHERE userId = ?", [userId])
+        .catch((err) => {
+        res.status(500).json({ Status: "Database Error" });
         console.error(err);
         return [];
-    });
+        });
 
-    return res.status(200).json({ isSuccenss: true, messge: "Payment Completed" });
+    return res.status(200).json({ isSuccess: true, message: "Payment Completed" });
 }
