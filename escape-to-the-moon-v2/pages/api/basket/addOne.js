74aabc5cf21c68fa0cbab3ddafa8c0f6d6a03f85
keegran <keegran191@gmail.com>
2023-08-06 21:59:07 +0700

    api +1 -1  , UwU

---
+++ escape-to-the-moon-v2/pages/api/basket/addOne.js
@@ -0,0 +1,31 @@
+import pool from "../../../lib/database";
+
+export default async function handler(req, res) {
+    const id = req.query.id;
+
+    try {
+        // Check if the combination of id already exists in the basket table.
+        const [existingRows] = await pool.query(
+            'SELECT * FROM basket WHERE id = ?',
+            [id]
+        );
+
+        if (existingRows.length > 0) {
+            // If the combination exists, decrement the stockAmount for that row.
+            await pool.query(
+                'UPDATE basket SET stockAmount = stockAmount + 1 WHERE id = ?',
+                [id]
+            );
+            pool.end();
+            res.status(200).json({ "Status": "StockAmount updated successfully." });
+        } else {
+            // If the combination doesn't exist, return an error response or any other desired behavior.
+            pool.end();
+            res.status(404).json({ "Status": "Combination not found in the basket." });
+        }
+    } catch (err) {
+        console.error(err);
+        pool.end();
+        res.status(500).json({ "Status": "Database Error" });
+    }
+}
