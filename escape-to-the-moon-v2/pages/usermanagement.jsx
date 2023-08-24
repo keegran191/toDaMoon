9c823640c2c9bfea4fc5b60f0391361e2c9302b7
M1zTerP <sawagan@hotmail.com>
2023-08-25 01:42:34 +0700

    Update usermanagement.jsx

--- escape-to-the-moon-v2/pages/usermanagement.jsx
+++ escape-to-the-moon-v2/pages/usermanagement.jsx
@@ -721,7 +721,7 @@ function UserManagement ({ cookies }) {
                                         <input onChange={(e) => {
                                             setAddressDetail(e.target.value)
                                         }} value={addressDetail} type="text" name="floating_email" id="floating_email" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-[#252525] dark:border-gray-600 dark:focus:border-[#252525] focus:outline-none focus:ring-0 focus:border-[#252525] peer" placeholder=" " required />
-                                        <label htmlFor="floating_email" className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-[#252525] peer-focus:dark:text-[#252525] peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">ลายละเอียดที่อยู่</label>
+                                        <label htmlFor="floating_email" className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-[#252525] peer-focus:dark:text-[#252525] peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">รายละเอียดที่อยู่</label>
                                     </div>
                                 </div>
 
@@ -850,7 +850,7 @@ function UserManagement ({ cookies }) {
                                         <input onChange={(e) => {
                                             setAddressDetail(e.target.value)
                                         }} value={addressDetail} type="text" name="floating_email" id="floating_email" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-[#252525] dark:border-gray-600 dark:focus:border-[#252525] focus:outline-none focus:ring-0 focus:border-[#252525] peer" placeholder=" " required />
-                                        <label htmlFor="floating_email" className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-[#252525] peer-focus:dark:text-[#252525] peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">ลายละเอียดที่อยู่</label>
+                                        <label htmlFor="floating_email" className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-[#252525] peer-focus:dark:text-[#252525] peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">รายละเอียดที่อยู่</label>
                                     </div>
                                 </div>
 
@@ -1070,7 +1070,7 @@ function UserManagement ({ cookies }) {
                                                     GetOrderItemByOrder(post.order_Id)
                                                 }}
                                             >
-                                                ดูลายละเอียด
+                                                ดูรายละเอียด
                                             </motion.button>
                                         </div>
                                     </div>
@@ -1131,7 +1131,7 @@ function UserManagement ({ cookies }) {
                                                             GetOrderItemByOrder(post.order_Id)
                                                         }}
                                                     >
-                                                        {`ดูลายละเอียดสินค้าทั้งหมด ->`}
+                                                        {`ดูรายละเอียดสินค้าทั้งหมด ->`}
                                                     </motion.p>
                                                 </div>
                                             </div>
