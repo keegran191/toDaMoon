bf7afb8547e2d83befc9310eeb6377a35f07f265
M1zTerP <sawagan@hotmail.com>
2023-08-27 00:01:30 +0700

    Update roast.jsx

--- escape-to-the-moon-v2/pages/roast.jsx
+++ escape-to-the-moon-v2/pages/roast.jsx
@@ -43,9 +43,9 @@ export default function Roast({ cookies }) {
                 <Nav name={fname} userid={userId} itemAmount={stockAmount.toString()}></Nav>
             </motion.div>
             <motion.div className='w-full h-full px-20'>
-                <motion.div className='hidden xl:flex w-full h-full items-center justify-center'>
+                <motion.div className='hidden 2xl:flex w-full h-full items-center justify-center'>
                     <motion.div 
-                        className='z-50 w-fit h-1/2 rounded-lg overflow-hidden'
+                        className='z-50 w-fit h-1/2 rounded-lg overflow-hidden shadow-xl'
                         initial={{ scale: 0 }}
                         animate={{ scale: 1 }}
                         transition={{
@@ -61,7 +61,6 @@ export default function Roast({ cookies }) {
                         <p className='text-xl mt-2'>&nbsp; สัมผัสประสบการณ์ที่หอมหวานและรสชาติที่เข้มข้นของกาแฟที่คั่วอย่างพิถีพิถันจากเมล็ดที่ดีที่สุดทั่วโลก ที่นี่ที่เราให้คุณเปิดโอกาสในการสัมผัสความสดชื่นของกาแฟที่เต็มไปด้วยความรสชาติที่เหมาะสำหรับทุกโอกาส</p>
                         <p className='text-xl mt-2'>&nbsp; เราใช้เทคนิคการคั่วที่ทันสมัยและการควบคุมอุณหภูมิอย่างเข้มงวดเพื่อให้ได้กาแฟที่มีความเข้มข้นที่สุดและรสชาติที่ยาวนาน ทุกลูกเมล็ดถูกคั่วด้วยความห่อหุ้มด้วยความห่อหุ้มด้วยความใส่ใจ ท่านจะรู้สึกถึงความแตกต่างในทุกๆครอบครัวของกาแฟที่คั่วโดยเรา</p>
                         <p className='text-xl mt-2'>&nbsp; อยากให้ชีวิตของคุณเติมเต็มด้วยกลิ่นหอมของกาแฟที่เข้มข้นและความรสชาติที่เหนือกว่าทุกคำบรรยาย สั่งซื้อเดี๋ยวนี้และเปลี่ยนวันทุกวันให้เป็นประสบการณ์ที่คุณจะต้องการที่จะทำซ้ำอีกครั้งแล้วและแล้ว</p>
-
                     </motion.div>
                 </motion.div>
 
