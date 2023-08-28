5d4ded80424e8d88f4a9561fdafd10c8bf6556ef
M1zTerP <sawagan@hotmail.com>
2023-08-28 22:47:48 +0700

    Update register.jsx

--- escape-to-the-moon-v2/pages/register.jsx
+++ escape-to-the-moon-v2/pages/register.jsx
@@ -22,7 +22,6 @@ export default function Login() {
     const [passwordErrorLenght, setPasswordErrorLenght] = useState(false)
     const [passwordConfirmError, setPasswordConfirmError] = useState(false)
     const [phoneError, setPhoneError] = useState(false)
-    const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
 
     return(
         <div>
@@ -46,7 +45,7 @@ export default function Login() {
                             className="block py-2.5 px-0 w-full text-lg text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-[#252525] dark:border-gray-600 dark:focus:border-[#252525] focus:outline-none focus:ring-0 focus:border-[#252525] peer" 
                             placeholder=" "
                             onChange={(e) => {
-                                if(emailRegex.test(e.target.value) == false && e.target.value != "") {
+                                if(e.target.value.includes('@')&& e.target.value.includes('.') && e.target.value != "") {
                                     setEmailValid(true);
                                 }
                             }}
