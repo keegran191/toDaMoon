60de778eae991b734514f22627e028878f413198
M1zTerP <sawagan@hotmail.com>
2023-08-29 20:32:16 +0700

    asdfasdfasdf

--- escape-to-the-moon-v2/server.js
+++ escape-to-the-moon-v2/server.js
@@ -1,6 +1,7 @@
 const { createServer } = require('http')
 const { parse } = require('url')
 const next = require('next')
+const { statSync } = require('fs')
  
 const dev = process.env.NODE_ENV !== 'production'
 const hostname = 'localhost'
@@ -16,8 +17,11 @@ app.prepare().then(() => {
       // This tells it to parse the query portion of the URL.
       const parsedUrl = parse(req.url, true)
       const { pathname, query } = parsedUrl
+
+      if (pathname == "/uploads") {
+        console.log(parsedUrl);        
+      }
  
-      console.log(pathname);
       await handle(req, res, parsedUrl)
     } catch (err) {
       console.error('Error occurred handling', req.url, err)
