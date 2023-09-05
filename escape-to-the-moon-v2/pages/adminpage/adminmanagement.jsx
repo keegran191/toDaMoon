2a9943ae8a5c4d6eb965066431238e71c1b39efb
M1zTerP <sawagan@hotmail.com>
2023-09-05 10:37:39 +0700

    Update adminmanagement.jsx

--- escape-to-the-moon-v2/pages/adminpage/adminmanagement.jsx
+++ escape-to-the-moon-v2/pages/adminpage/adminmanagement.jsx
@@ -772,7 +772,9 @@ function AdminManagement({ cookies }) {
                                         {adminOrder[index].orderShowStatusList && (
                                             <motion.div className='z-50 absolute w-full p-2 flex flex-col items-center top-12 bg-[#252525] rounded-lg'>
                                                 {orderStatusList.length > 0 && orderStatusList.map((status) => {
-
+                                                    return (
+                                                        <motion.div></motion.div>
+                                                    )
                                                 })}
                                             </motion.div>
                                         )}
