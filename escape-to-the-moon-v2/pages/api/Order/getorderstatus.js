3ff12bf5240e4611595ee4481765770151366bb4
M1zTerP <sawagan@hotmail.com>
2023-09-05 10:38:52 +0700

    Update getorderstatus.js

--- escape-to-the-moon-v2/pages/api/Order/getorderstatus.js
+++ escape-to-the-moon-v2/pages/api/Order/getorderstatus.js
@@ -3,7 +3,7 @@ import db from "../../../lib/database";
 export default async function handler(req, res) {
   const pool = await db.getConnection();
 
-  const [results] = await pool.query('SELECT * FROM order_status').catch((err) => {
+  const [results] = await pool.query('SELECT * FROM order_status WHERE id != 4').catch((err) => {
     res.status(500).json({ "Status": "Database Error" });
     console.error(err);
     return null;
