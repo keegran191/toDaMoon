19244d138057c0d0c27cf18a24b9ef3fffb5bb6c
M1zTerP <sawagan@hotmail.com>
2023-09-05 21:12:56 +0700

    Update adminmanagement.jsx

--- escape-to-the-moon-v2/pages/adminpage/adminmanagement.jsx
+++ escape-to-the-moon-v2/pages/adminpage/adminmanagement.jsx
@@ -848,8 +848,8 @@ function AdminManagement({ cookies }) {
                                         >
                                             บันทึกรายการ
                                         </motion.button>
-                                        <svg className='cursor-pointer w-8 h-8 fill-[#009e9e] ml-3' xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 512 512">
-                                            <path d="M256 512A256 256 0 1 0 256 0a256 256 0 1 0 0 512zM216 336h24V272H216c-13.3 0-24-10.7-24-24s10.7-24 24-24h48c13.3 0 24 10.7 24 24v88h8c13.3 0 24 10.7 24 24s-10.7 24-24 24H216c-13.3 0-24-10.7-24-24s10.7-24 24-24zm40-208a32 32 0 1 1 0 64 32 32 0 1 1 0-64z"/>
+                                        <svg className='cursor-pointer w-8 h-8 fill-[#252525] ml-3' xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 512 512">
+                                            <path d="M416 208c0 45.9-14.9 88.3-40 122.7L502.6 457.4c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L330.7 376c-34.4 25.2-76.8 40-122.7 40C93.1 416 0 322.9 0 208S93.1 0 208 0S416 93.1 416 208zM208 352a144 144 0 1 0 0-288 144 144 0 1 0 0 288z"/>
                                         </svg>
                                     </motion.div>
                                 </motion.div>
