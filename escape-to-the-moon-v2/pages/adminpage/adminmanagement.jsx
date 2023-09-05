2d4818ca82140ece620145888574f5ccf30ecfa0
M1zTerP <sawagan@hotmail.com>
2023-09-05 10:59:43 +0700

    Update adminmanagement.jsx

--- escape-to-the-moon-v2/pages/adminpage/adminmanagement.jsx
+++ escape-to-the-moon-v2/pages/adminpage/adminmanagement.jsx
@@ -787,8 +787,8 @@ function AdminManagement({ cookies }) {
                                                         opacity: 1,
                                                         scale: 1,
                                                         transition: {
-                                                          delayChildren: 0.3,
-                                                          staggerChildren: 0.2
+                                                          delayChildren: 0.1,
+                                                          staggerChildren: 0.1
                                                         }
                                                     }
                                                 }}
