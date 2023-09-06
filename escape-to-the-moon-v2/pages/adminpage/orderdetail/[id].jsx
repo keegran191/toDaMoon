1e3ea520d239b77d3b9c2c52868c1897fb518b38
M1zTerP <sawagan@hotmail.com>
2023-09-06 21:58:13 +0700

    Update [id].jsx

--- escape-to-the-moon-v2/pages/adminpage/orderdetail/[id].jsx
+++ escape-to-the-moon-v2/pages/adminpage/orderdetail/[id].jsx
@@ -114,7 +114,7 @@ function OrderDetail({ cookies }) {
                                 <motion.div className='w-auto'>
                                     <p className='text-2xl'>รูปแบบการชำระเงิน</p>
                                     <p className='text-lg'>QR Payment</p>
-                                    <motion.div className='w-96 h-20 border-2 border-[#252525] flex items-center px-3'>
+                                    <motion.div className='w-96 h-10 border-2 border-[#252525] flex items-center px-3'>
                                         <svg className='w-5 h-5 fill-[#0FC000]' xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 512 512">
                                             <path d="M256 512A256 256 0 1 0 256 0a256 256 0 1 0 0 512zM369 209L241 337c-9.4 9.4-24.6 9.4-33.9 0l-64-64c-9.4-9.4-9.4-24.6 0-33.9s24.6-9.4 33.9 0l47 47L335 175c9.4-9.4 24.6-9.4 33.9 0s9.4 24.6 0 33.9z"/>
                                         </svg>
