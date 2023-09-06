51f8a6317e5d33a78790c3f696f766e56488f3b1
M1zTerP <sawagan@hotmail.com>
2023-09-06 23:14:25 +0700

    Update [id].jsx

--- escape-to-the-moon-v2/pages/adminpage/orderdetail/[id].jsx
+++ escape-to-the-moon-v2/pages/adminpage/orderdetail/[id].jsx
@@ -168,13 +168,13 @@ function OrderDetail({ cookies }) {
                             <motion.div className='flex justify-center mt-10'>
                                 <motion.div className='w-6/12 h-auto'>
                                     <p className='text-2xl'>รายการสั่งซื้อ</p>
-                                    <motion.div className='w-full h-1 border-b-2 border-[#252525] text-center'></motion.div>
+                                    <motion.div className='w-full h-1 border-b-2 border-[#252525]'></motion.div>
                                     
                                     <motion.div className='w-full grid grid-cols-5 px-5 py-3 xl:py-3 lg:px-10'>
-                                        <p className='text-base col-span-2'>สินค้า</p>
-                                        <p className='text-base col-start-3'>ราคา</p>
-                                        <p className='text-base'>จำนวน</p>
-                                        <p className='text-base'>ราคารวม</p>
+                                        <p className='text-center text-base col-span-2'>สินค้า</p>
+                                        <p className='text-center text-base col-start-3'>ราคา</p>
+                                        <p className='text-center text-base'>จำนวน</p>
+                                        <p className='text-center text-base'>ราคารวม</p>
                                     </motion.div>
                                     
                                     <motion.div className='w-full h-1 border-b-2 border-[#252525]'></motion.div>
